56e5d838cb4c07ab39221c2527824c9973b0ba5f
Jacopo Pauletto <pauletto.jaco@gmail.com>
2022-04-07 10:33:01 +0200

    sfs script

--- sfs_read.py
+++ sfs_read.py
@@ -1,4 +1,3 @@
-
 import sys
 import inspect
 
@@ -6,15 +5,15 @@ program = sys.argv[0]
 path_to_sfs = sys.argv[1] 
 
 
-
 inFile = open(sys.argv[1], 'r')
 sfs_dict = dict()
 t = tuple()
-i = 1 
+str = ''
 for line in inFile :
     a,b,c,d,e = line.split()
-    t = (a,b,c,d,e)
-    sfs_dict[i] = t
-    i = i+1
+    t = (b,c,d,e)
+    if a != '*' :
+        str = a
+        sfs_dict[str] = []
+    sfs_dict[str].append(t) 
 inFile.close()
-
