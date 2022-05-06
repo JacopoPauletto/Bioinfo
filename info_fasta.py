9b3cbcbfdc18a518bc5b88001b1b05c622159ec8
JacopoPauletto <101558269+JacopoPauletto@users.noreply.github.com>
2022-05-06 11:54:45 +0200

    Update info_fasta.py

--- info_fasta.py
+++ info_fasta.py
@@ -16,6 +16,8 @@ sum = 0
 max_len = 0
 min_len = sys.maxsize
 array_of_len = []
+discard = 0
+keep = 0
 
 inFile = open(sys.argv[1], "r") 
 for record in SeqIO.parse(inFile, "fasta"):
@@ -26,16 +28,22 @@ for record in SeqIO.parse(inFile, "fasta"):
         max_len = len(record.seq)
    if len(record.seq) <= min_len : 
         min_len = len(record.seq)
+   if len(record.seq) < int(sys.argv[2]) :
+        discard  = discard + 1
+   else :
+        keep = keep + 1
 
 
 print("Number of entry:", entry_counter)
 print("Maximum length:", max_len)
 print("Minimum length:", min_len)
 print("Average length:", sum/entry_counter)
+print("Number of sequences minor of " + sys.argv[2] + " = ", discard)
+print("Number of sequences greater than " + sys.argv[2] + " = ", keep)
 
-
-
+plt.style.use('ggplot')
 plt.xlabel("sequence length")
 plt.ylabel("number of sequences")
-plt.hist(array_of_len, bins = entry_counter)
+w = 1
+plt.hist(array_of_len, edgecolor = 'black',  bins = np.arange(min(array_of_len),max(array_of_len)+w, w))
 plt.show()
